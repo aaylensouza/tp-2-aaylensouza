8e18a080218579bd3d8bab096023946f998e3201
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-03-25 22:09:06 -0300

    entrega change.py

--- change.py
+++ change.py
@@ -1,3 +1,9 @@
 def change():
     expense = 23.75
     money = 100
+    print(f"{"Ingresar gasto: "}"+ " " + f"\n{expense}")
+    print(f"{"Dinero recibido: "}"+ " " + f"\n{money}")
+    print(f"{"Vuelto: "}"+ " " + f"\n{""}")
+    print(f"{"Pesos: "}"+ " " + f"\n{int(money - expense)}")
+    print(f"{"Centavos: "}"+ " " + f"\n{(money - expense) - int(money - expense)}")
+    
